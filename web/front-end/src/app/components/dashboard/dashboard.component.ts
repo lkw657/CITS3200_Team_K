8a178baab92824f1865bce8fa3281ec49d350065
Mac Ahmed <chmaktoomahmed@gmail.com>
2018-09-05 20:40:03 +0800

    Updated User Interface for Staff Dashboard
    created staff dashboard, deleted dummy user data

--- web/front-end/src/app/components/dashboard/dashboard.component.ts
+++ web/front-end/src/app/components/dashboard/dashboard.component.ts
@@ -9,31 +9,6 @@ export class DashboardComponent implements OnInit {
 
   constructor() { }
 
-  user = {
-    "role": "student",
-    "name": "James Bond",
-    "email": "james.bond@student.uwa.edu.au",
-    "number": "21728599",
-    "password" : "92df2d6556fb2ac5689c4506f2b95532",
-    "forms": [{
-      "owner": "James Bond",
-      "created_date": "10/08/2018",
-      "status": "Approved",
-      "comments": {
-        "commenter": "Bruce Wayne",
-        "comment": "Needs some more work - Dick was better."
-      }
-    },{
-      "owner": "James Bond",
-      "created_date": "15/08/2018",
-      "status": "Awaiting HoS",
-      "comments": {
-        "commenter": "Clark Kent",
-        "comment": "Needs some more work - Conner was better."
-      }
-    }],
-  };
-
   ngOnInit() {
   }
 
