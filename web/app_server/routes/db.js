e46b474fbb2fcc72e8cdade40b9e581b2e709f84
Dave <dweight13@hotnail.com>
2018-09-05 16:31:53 +0800

    Auth service initial creation
    The auth service and login/logout sections are 90% complete.  I have
    added to db.js suggestions for what I need the back end to send me

--- web/app_server/routes/db.js
+++ web/app_server/routes/db.js
@@ -2,12 +2,58 @@ var express = require('express');
 var router = express.Router();
 var ctrlUsers = require('../controllers/users.js')
 
-router.get('/user/:userid', function(req, res, next) {
-    res.status('200');
-    res.json({'username': 'lala'});
-  });
+router.get('/user/:userid', function (req, res, next) {
+  res.status('200');
+  res.json({ 'username': 'lala' });
+});
 
 /* GET users listing. */
 router.get('/user', ctrlUsers.respond);
+
+//User Register Route placeholder
+//This will receive an object containining the below - 
+// role: req.body.role,
+// email: req.body.email,
+// username: req.body.username,
+// password: req.body.password
+
+//The controller needs to ensure the username and email are not already in db then add to db if all good then return as below 
+
+router.post('/register', function (req, res, next) {
+
+  //If all ok
+  res.json({ success: true, msg: 'User Registered' });
+
+  //If failed
+  res.json({ success: false, msg: 'Failed to Register User' });
+});
+
+
+
+//User Authenticate Route
+//This will receive an object containining the below - 
+// username: req.body.username,
+// password: req.body.password
+
+//Controller will need to make sure username in db then compare password then return as below
+
+router.post('/authenticate', function (req, res, next) {
+
+  //If successful
+  res.json({
+    success: true,
+    msg: 'You are successfully logged in',
+    //token: `JWT ${token}` , This is what I used in my project, any token would do I guess? 
+    user: {
+      username: user.username
+    }
+  });
+
+  //If username not in db
+  return res.json({ success: false, msg: 'User not found' });
+
+  //If password wrong
+  return res.json({ success: false, msg: 'Password incorrect' });
+});
 
 module.exports = router;
