606241beb60c6a119667d5333afe95aa8f032e61
Neosh Sheikh <neosh11@gmail.com>
2018-09-05 17:42:01 +0800

    When a form is added, it's ref is also added to the owner

--- web/app_server/controllers/forms.js
+++ web/app_server/controllers/forms.js
@@ -1,6 +1,8 @@
 var formModel = require('../models/forms');
 var Form = formModel.formSchema;
 
+var userModel = require('../models/users');
+var User = userModel.userSchema;
 
 /**
 req.body.owner
@@ -11,29 +13,55 @@ Assign status
 module.exports.addForm = (req, res, next)=>{
     if(req.body.owner && req.body.questionSet)
     {
-      var form = new Form();
+        User.findById(req.body.owner, (err, owner)=>{
+
+            if(err){
+                sendJsonResponse(res, 400, {
+                    error: err
+                });
+            }
+            else if(!owner){
+                sendJsonResponse(res, 400, {
+                    error: "user not found"
+                });
+            }
+            else{
+                var form = new Form();
   
-      form.owner=req.body.owner;
-      form.questionSet=req.body.questionSet;
-      form.status='created';
-  
-      form.save((err,form)=>{
-        if(err){
-          sendJsonResponse(res, 400, {
-              error: err
-          });
-        }
-        else{
-          sendJsonResponse(res, 201, form);
-        }
-      })
+                form.owner=req.body.owner;
+                form.questionSet=req.body.questionSet;
+                form.status='created';
+            
+                form.save((err,form)=>{
+                    if(err){
+                        sendJsonResponse(res, 400, {
+                            error: err
+                        });
+                    }
+                    else{
+                        owner.forms.push(form._id);
+                        owner.save((err, owner)=>{
+                            if(err){
+                                sendJsonResponse(res, 400, {
+                                    error: err
+                                });
+                            }
+                            else{
+                                sendJsonResponse(res, 201, form);
+                            }
+                        })
+                        
+                    }
+                });
+            }
+        });
     }
     else
     {
-      sendJsonResponse(res, 400, {
-          error: "missing data"
-      });
-    }
+        sendJsonResponse(res, 400, {
+            error: "missing data"
+        });
+    } 
   }
 
 module.exports.listAll = (req, res, next) => {
