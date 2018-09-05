f04566c27da2642ff4ef716095b006252ce22595
neosh11 <neosh11@gmail.com>
2018-09-05 20:25:52 +0800

    Moved '/' under '/db' and '/mail'
    In case the future holds conflicts over what route to use.

--- web/app.js
+++ web/app.js
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/db', dbRouter);
 app.use('/mail', mailRouter);
+app.use('/', indexRouter);
 
 module.exports = app;
