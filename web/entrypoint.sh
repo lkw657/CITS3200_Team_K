7fe138b5b10734e3d66c32c7de3e6e6b9cd36443
lkw657 <lkw657@users.noreply.github.com>
2018-09-24 06:17:02 +0000

    Fix docker for travis

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -4,6 +4,8 @@ if [[ $1 == "debug" ]]; then
     npm install
     nodemon bin/www
 elif [[ $1 == "test" ]]; then
+    npm install
+    npm install -g mocha
     mocha -R spec --exit
 else
     node bin/www
