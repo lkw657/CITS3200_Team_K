a44628c9bc0cfe9ef435b8981d79103910087074
Dave Weight <21930719@student.uwa.edu.au>
2018-09-25 18:53:22 +0800

    Controller to retrieve latest complete, minor front end changes
    Back end controller to retrieve latest version  of question set completed, fixed incorrect rendering of currentQuestion.type

--- web/app_server/controllers/questionSets.js
+++ web/app_server/controllers/questionSets.js
@@ -1,58 +1,58 @@
 var questionSetModel = require('../models/questionSets');
 var QuestionSet = questionSetModel.questionSetSchema;
 
-module.exports.addQuestionSet = (req, res, next)=>{
-    if(req.body.questionList)	//Check if a questionlist is included.
+module.exports.addQuestionSet = (req, res, next) => {
+    if (req.body.questionList)	//Check if a questionlist is included.
     {
-		QuestionSet.findOne({},{_id: 0,'questionList._id':0}).sort({version:-1}).then(function(qset){	//Find the question set with the highest version.
-			var questionSet = new QuestionSet();	//The question set to be saved.
-			//If no questionSet was returned earlier then set the version to 1. Otherwise the version is 1 greater than the latest.
-			if (qset)	{	
-				if(isEqual(qset.questionList,req.body.questionList))
-					return res.json({ success: false, msg: 'Same Question List' });	//If the latest question list is identical to the new question list then abort.
-				questionSet.version = qset.version+1;
-			}
-			else {
-				questionSet.version = 1;
-			}
-			questionSet.questionList=req.body.questionList;
-			questionSet.save((err, questionSet) => {
-				if (err) {
-					return res.json({ success: false, msg: 'Could not update Question Set' });
-				}
-				else {
-					return res.json({ success: true, msg: 'Question Set updated!' });
-				}
-			})
-		});
-		}
+        QuestionSet.findOne({}, { _id: 0, 'questionList._id': 0 }).sort({ version: -1 }).then(function (qset) {	//Find the question set with the highest version.
+            var questionSet = new QuestionSet();	//The question set to be saved.
+            //If no questionSet was returned earlier then set the version to 1. Otherwise the version is 1 greater than the latest.
+            if (qset) {
+                if (isEqual(qset.questionList, req.body.questionList))
+                    return res.json({ success: false, msg: 'Same Question List' });	//If the latest question list is identical to the new question list then abort.
+                questionSet.version = qset.version + 1;
+            }
+            else {
+                questionSet.version = 1;
+            }
+            questionSet.questionList = req.body.questionList;
+            questionSet.save((err, questionSet) => {
+                if (err) {
+                    return res.json({ success: false, msg: 'Could not update Question Set' });
+                }
+                else {
+                    return res.json({ success: true, msg: 'Question Set updated!' });
+                }
+            })
+        });
+    }
     else
-      return res.json({ success: false, msg: 'Missing Data' });
+        return res.json({ success: false, msg: 'Missing Data' });
 }
 
 //Checks if the two given questionList arrays are the same, regardless of order.
 var isEqual = function (first, second) {
-	if(first.length!=second.length)	//Check if the arrays are of the same length.
-		return false;
-	first.sort(	//sort based on question title.
-		function(a, b){
-		var x = a.title.toLowerCase();
-		var y = b.title.toLowerCase();
-		if (x < y) {return -1;}
-		if (x > y) {return 1;}
-		return 0;
-	});
-	second.sort(	//sort based on question title.
-		function(a, b){
-		var x = a.title.toLowerCase();
-		var y = b.title.toLowerCase();
-		if (x < y) {return -1;}
-		if (x > y) {return 1;}
-		return 0;
-	});
-	if(JSON.stringify(first)!=JSON.stringify(second))	//Check if sorted arrays are the same.
-		return false;
-	return true;	// If nothing failed, return true
+    if (first.length != second.length)	//Check if the arrays are of the same length.
+        return false;
+    first.sort(	//sort based on question title.
+        function (a, b) {
+            var x = a.title.toLowerCase();
+            var y = b.title.toLowerCase();
+            if (x < y) { return -1; }
+            if (x > y) { return 1; }
+            return 0;
+        });
+    second.sort(	//sort based on question title.
+        function (a, b) {
+            var x = a.title.toLowerCase();
+            var y = b.title.toLowerCase();
+            if (x < y) { return -1; }
+            if (x > y) { return 1; }
+            return 0;
+        });
+    if (JSON.stringify(first) != JSON.stringify(second))	//Check if sorted arrays are the same.
+        return false;
+    return true;	// If nothing failed, return true
 
 };
 
@@ -200,46 +200,15 @@ module.exports.questionSetId = (req, res, next) => {
 
 //Returns the latest 
 module.exports.latestQuestionSet = (req, res, next) => {
-    //FOR TESTING
-    res.json(
-        {
-            _id: "5b964db01e7bd3273c8f66c1",
-            questionList: [
-                {
-                    _id: "5b964db01e7bd3273c8f66c4",
-                    text: "Provide a brief statement outlining how your proposal aligns with the Faculty’s Strategic Vision.",
-                    title: "ALIGNMENT WITH FACULTY STRATEGIC VISION",
-                    type: "textarea",
-                    formName: "Faculty"
-                },
-                {
-                    _id: "5b964db01e7bd3273c8f66c3",
-                    text: "Provide a brief statement outlining how your proposal aligns with the Faculty’s strategic research priorities (Engineering for Remote Operations, Science of Discovery or Technologies for Better Health) or an emerging strategic research priority.",
-                    title: "ALIGNMENT WITH FACULTY STRATEGIC RESEARCH PRIORITIES",
-                    type: "textarea",
-                    formName: "Faculty"
-                },
-                {
-                    _id: "5b964db01e7bd3273c8f66c2",
-                    text: "Please provide a brief statement as to how this investment will return value to the Faculty.",
-                    title: "RETURN ON INVESTMENT",
-                    type: "textarea",
-                    formName: "Faculty"
-                }
-            ],
-            version: 1,
-            latest: true,
-            __v: 0
-        }
-    );
-    //END OF TESTING
-    /*
-        //Actual code. 
-        //TODO: Seems to work. Check if this is right.
-        QuestionSet.findOne().sort({version:-1}).then(function(qset){
-            res.send(qset);
-        })
-    */
+    //Find the question set with the highest version.
+    QuestionSet.findOne({}, { _id: 0, 'questionList._id': 0 }).sort({ version: -1 }).then(function (qset, err) {
+        if (err)
+            return sendJsonResponse(res, 400, {
+                error: err
+            });
+
+        return sendJsonResponse(res, 200, qset);
+    });
 }
 
 
