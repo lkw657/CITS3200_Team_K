d555dbbaa62df804cc459fc442b5a59ddca8b905
Dave Weight <21930719@student.uwa.edu.au>
2018-09-27 14:45:52 +0800

    New Submission service

--- web/front-end/src/app/services/question.service.ts
+++ web/front-end/src/app/services/question.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { baseURI } from '../config';
 
 @Injectable()
 export class QuestionService {
 
     HTTP: HttpClient;
-    
-    constructor(private http: HttpClient){
+
+    constructor(private http: HttpClient) {
         this.HTTP = http;
     }
 
-    getData(){
-        return this.HTTP.get("http://localhost:3000/db/questionSet/latest");
+    getData() {
+        return this.HTTP.get(baseURI + '/db/questionSet/latest');
+    }
+
+    newSubmission(form) {
+        return this.http.post<any>(baseURI + '/db/newSubmission', form);
+    }
+
+    updateSubmission(form) {
+        return this.http.post<any>(baseURI + '/db/updateSubmission', form);
     }
 }
