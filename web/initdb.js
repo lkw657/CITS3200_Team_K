58b2f9b883e87828392cbce6525ef930a9564e1f
lkw657 <lkw657@users.noreply.github.com>
2018-09-27 12:37:27 +0000

    Added some forms to initdb for testing

--- web/initdb.js
+++ web/initdb.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 require('./app_server/models/db'); 
 var QuestionSet = require('./app_server/models/questionSets').questionSetSchema;
 var User  = require('./app_server/models/users').User;
+var Form  = require('./app_server/models/forms').Form;
 
 function readSync(question, silent=false) {
     return new Promise((resolve, reject) => {
@@ -41,7 +42,19 @@ async function run() {
     qset = JSON.parse(fs.readFileSync('questionSet.json', 'utf8'));
     for (var i = 0; i<qset.questionList.length; i++)
         qset.questionList[i].order = i;
-    await new QuestionSet(qset).save().catch((err) => console.log(err));
+    qset = await new QuestionSet(qset).save().catch((err) => console.log(err));
+
+    answers = []
+    answers2 = []
+    for (var i=0; i<qset.questionList.length; i++) {
+        answers.push({order:i, answer:"foo"});
+        answers2.push({order:i, answer:"bar"});
+    }
+    var submission = await new Form({owner: user._id, dates:[new Date()], questionSet: qset, answers: answers, status:'awaiting-hos', school:'idk', submitter:'researcher'}).save().catch((err) => console.log(err));
+    var approval = await new Form({owner: user._id, dates:[new Date()], questionSet: qset, answers: answers2, status:'awaiting-hos', school:'idk', submitter:'researcher'}).save().catch((err) => console.log(err));
+    user.submissions = [submission._id]
+    user.approvals = [approval._id]
+    await user.save().catch((err) => console.log(err));
 
     process.exit()
 }
