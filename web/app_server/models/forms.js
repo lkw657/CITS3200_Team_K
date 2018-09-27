3345e70020237d3b2995874f3656f020060170a7
neosh <neosh11@gmail.com>
2018-09-27 12:27:13 +0800

    Updates to schema
    change schema of user, form -> (submission, approvals)
    change schema of form, history -> Array of IDs
    change schema of form, comment -> array of comment objects (questionNumber, comment, commenter)
    change schema of form, approvedBy -> array (role, id)
    order field to questionSet

--- web/app_server/models/forms.js
+++ web/app_server/models/forms.js
@@ -1,5 +1,20 @@
 mongoose = require('mongoose')
 
+var approvedBySchema = new mongoose.Schema(
+    {
+        role: {type: String, required: true},	//Title of the question
+        id: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},	//Body text for the question
+    }
+);
+
+var commentSchema = new mongoose.Schema(
+    {
+        questionNumber: {type: Number, required: true},	//Title of the question
+        comment: {type: String, ref: 'User', required: true},	//Body text for the question
+        commenter: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}
+    }
+);
+
 var formSchema = new mongoose.Schema(
     {
     owner: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},	//The researcher
@@ -8,13 +23,11 @@ var formSchema = new mongoose.Schema(
     answers: [{ type: String, required: true }],	//Researcher's answers to the questions in the question set.
     allocatedStaff: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},	//Users who are acting as HoS, AD(R), etc. for this form. 
     status: {type: String, required: true},	//If the form is completed, rejected, etc.
-    history: {type: mongoose.Schema.Types.ObjectId, ref:'Form'},	//Old versions of the form.
-    approvedBy: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],	//Users (HoS, etc.) who have approved the form.
-    comments: {	//Comments for provisional yes/no.
-        commenter: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
-        comment: String
-    },
-	school: {type: String, required: true}	//What school the researcher belongs to, determines HoS to send to.
+    history: [{type: mongoose.Schema.Types.ObjectId, ref:'Form'}],	//Old versions of the form.
+    approvedBy: [{type: approvedBySchema}],	//Users (HoS, etc.) who have approved the form.
+    comments: [{type: commentSchema}],
+    school: {type: String, required: true},	//What school the researcher belongs to, determines HoS to send to.
+    submitter: {type: String, required: true}
     }
 );
 
--- web/app_server/models/questionSets.js
+++ web/app_server/models/questionSets.js
@@ -5,7 +5,8 @@ var questionSchema = new mongoose.Schema(
         title: {type: String, required: false},	//Title of the question
         text: {type: String, required: true},	//Body text for the question
         type: {type: String, required: true},	// types like textarea, text, date, money, number
-        formName: {type: String, required: true}	//What form the question belongs to (faculty or central)
+        formName: {type: String, required: true},	//What form the question belongs to (faculty or central)
+        order: {type: Number, required: true}
     }
 );
 
