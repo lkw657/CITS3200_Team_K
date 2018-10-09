7132ebb650b1d3273926997678df3398f968a1dc
Dave Weight <21930719@student.uwa.edu.au>
2018-10-09 22:40:03 +0800

    Fixed logout/login

--- web/front-end/src/app/guards/auth.guard.ts
+++ web/front-end/src/app/guards/auth.guard.ts
@@ -18,6 +18,7 @@ export class AuthGuard implements CanActivate{
     }
     else{
       this.router.navigate(['']);
+      localStorage.clear();
       this.flashMessage.show('Please login to access page', { cssClass: 'align-top alert alert-danger', timeout: 5000 });
       return false;
     }
