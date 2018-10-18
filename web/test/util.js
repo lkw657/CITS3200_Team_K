cde0430703b74d07f8403da39149180bba5d6584
lkw657 <lkw657@users.noreply.github.com>
2018-10-18 15:42:17 +0800

    Updated tests

--- web/test/util.js
+++ web/test/util.js
@@ -1,13 +1,14 @@
 var User = require('../app_server/models/users.js').User
 
 module.exports.makeUser = (fname, lname, number, password, isIT, callback) => {
-    User.register(User.create(fname, lname, number),
+    User.register(User.create(fname, lname, number, number+'@uwa.edu.au'),
                   password,
                   (err, user) => {
-                      if (err) {
+                      if (err || !user) {
+                          console.log(err);
                           callback(err, user);
-                        }
-                      if (isIT) {
+                      }
+                      else if (isIT) {
                           user.isIT = true;
                           user.save(callback);
                       }
